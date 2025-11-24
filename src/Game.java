9ed2f25b96882b575faf828d47ca96f0515afe7e
kenzaben19 <kben19@hotmail.com>
2025-11-24 09:28:46 -0600

    trying to figure how to add background

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,17 @@
+import java.io.File;
+
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Image;
 
 public class Game {
+    private File backgroundFile = new File("Gemini_Generated_Image_o2bfmoo2bfmoo2bf");
     private static CanvasWindow CANVASWINDOW;
+    private Image background = new Image("Gemini_Generated_Image_o2bfmoo2bfmoo2bf");
+    private Slingshot slingshot;
+    private Coo currentCoo;
+    private Level level;
+    private Handler handler;
+    private boolean isDragging;
+
+    
 }
